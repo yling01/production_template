f287e244be1facd3ce7e53d8c261e3b849ef4b2c
Tim Ling <tim_ling@Tims-iMac.local>
2020-07-05 23:48:55 -0400

    update

--- 1pdb2gmx/s1/mover.sh
+++ 1pdb2gmx/s1/mover.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dir=s1
-cp cx_amber99sbMod_tip3p.top cp ../../amber2RSFF/${dir}/
+cp cx_amber99sbMod_tip3p.top ../../amber2RSFF/${dir}/
 cp posre.itp ../../2*/${dir}
 cp prot.gro ../../2*/${dir}
 cp posre.itp ../../6*/${dir}
